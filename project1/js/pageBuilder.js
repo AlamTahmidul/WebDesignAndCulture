29a0f7ab1c37eb401a659e697dc7806114f6dd00
Tahmidul Alam <31861624+AlamTahmidul@users.noreply.github.com>
2022-09-22 18:05:54 -0400

    draft 01 submission

--- project1/js/pageBuilder.js
+++ project1/js/pageBuilder.js
@@ -33,7 +33,8 @@ function buildPage(pageName, state) {
     // Set the currentPage to whatever the "state" was passed in
     currentPage = state;
 
-    createInteractive("#intro");
+    if (currentPage < 100)
+        createInteractive("#intro");
 
     // console.log($('body'));
     // if ($(this).css('background-color') === 'white') {
@@ -42,23 +43,11 @@ function buildPage(pageName, state) {
     // console.log(currentPage);
     switch (state) {
         case 1:
-            menu(pageName);
-            break;
         case 2:
-            menu(pageName);
-            break;
         case 3:
-            menu(pageName);
-            break;
         case 4:
-            menu(pageName);
-            break;
         case 5:
-            menu(pageName);
-            break;
         case 6:
-            menu(pageName);
-            break;
         case 7:
             menu(pageName);
             break;
@@ -68,12 +57,15 @@ function buildPage(pageName, state) {
         case 11:
             eventManagement(pageName, state);
             break;
+        case 100:
+            end();
+            break;
         default:
             menu(pageName);
     }
 }
 
-function createInteractive(tag, p = '#pageHeader_Content') {
+function createInteractive(tag, p = '#pageHeader_Content', a = arr) {
     // Add Interactive Element at the end of the paragraph...
     const interactive = 
     `<div id="interactive">
@@ -84,8 +76,12 @@ function createInteractive(tag, p = '#pageHeader_Content') {
     $('#interactive input').click(function() {
         const parent = $(p);
         i += 1;
-        let arrIndex = currentPage >= arr.length ? Math.floor(Math.random()*arr.length) : currentPage;
-        renderText(arr[arrIndex], i, parent);
+        let arrIndex = currentPage >= a.length ? Math.floor(Math.random()*a.length) : currentPage;
+        if (currentPage >= 100) {
+            renderText(a, i, parent);
+        } else {
+            renderText(a[arrIndex], i, parent);
+        }
     });
 }
 
@@ -109,23 +105,31 @@ const arr = [
     [
         "It was on the left this entire time...",
         "This is the last time.",
-        "After this, this is game over...",
-        ""
+        "After this...",
+        "... game over...",
     ]
 ];
+
+const endNoEscape = [
+    "...", "...", "...",
+    "This-",
+    "Can't...",
+    "...be-",
+    "...happening-",
+    "..."
+]
 
 let randomWords = [];
 
 function menu(pageName) {
-    if (Math.floor(Math.random()*100) < 30)
+    if (Math.floor(Math.random()*100) < 30 || currentPage === -1)
     {
         $('body').last().addClass('_breatheBg');
         $('#intro').last().addClass('_breathe');
     }
     // TODO: The variable 'sane' is for the sane level that the player has. Lower the number, the more unstable the character is
-    const sane = 80;
-    if (Math.floor(Math.random()*100) < sane && currentPage != -1) {
-        // spawnWords($('#intro'));
+    const sane = 0;
+    if (Math.floor(Math.random()*100) >= sane && currentPage != -1) {
         loadData($('#intro'));
     }
 }
@@ -135,7 +139,7 @@ function renderText(arr, index, parent) {
         contDiv();
         return;
     }
-    if (index < arr.length) {
+    if (index < arr.length || index >= 100) {
         parent.append(arr[index] + '</br>');
     } else {
         contDiv();
@@ -153,7 +157,9 @@ function contDiv() {
     /* Logic is this: Put ./html/ if the current Page is index.html otherwise, go the corresponding "chapter" */
     let href = `${currentPage < 0 ? "./html/" : "./"}chapter${currentPage < 0 ? 1 : (currentPage + 2)}.html`;
     if (currentPage == 7) {
-        href = "../index.html";
+        href = "./endNoEscape.html";
+    } else if (currentPage === 100) {
+        href = "./../index.html";
     }
 
     /* Normal Routing to narratives/index */
@@ -162,40 +168,55 @@ function contDiv() {
         window.location.href = href;
     });
     // Change Text
-    $('#interactive input')[0].value = 'Next ->';
+    $('#interactive input')[0].value = currentPage === 100 ? "Start Over?" : 'Next ->';
 }
 
 function eventManagement(pageName, state) {
     const event = $('#event');
     loadData(event);
     if (state == 11) {
-
+        console.log("EVENT PAGE!");
     }
 }
 
-function spawnWords(tag, randomWords, fill = false) {
-    const poolLength = Math.floor(Math.random() * randomWords.length);
+function spawnWords(tag, randomWords) {
+    let poolLength = Math.floor(Math.random() * (randomWords.length - 1)) + 1;
+    console.log(poolLength);
     const randomButton = Math.floor(Math.random() * poolLength);
     const randomEvent = Math.floor(Math.random() * 4) + 1;
     // console.log(poolLength);
-    for (let i = 0 ; i < poolLength; i++) {
+    console.log(randomWords)
+    for (let j = 0 ; j <= poolLength; j++) {
         /* Create Buttons that represent leaking "thoughts" */
         // Define a random position on screen
-        var posX = Math.floor(Math.random() * 101);
-        var posY = Math.floor(Math.random() * 101);
+        const posX = Math.floor(Math.random() * 101);
+        const posY = Math.floor(Math.random() * 101);
+        const value = randomWords[Math.floor(Math.random()*(randomWords.length - 1))];
         // console.log("POSX: " + posX + ", POSY: " + posY);
-        const button = `<input 
+        const button = `
+        <input 
             type='button'
+            value="${value}"
             style= 
                 '
+                    position: absolute;
                     transform: rotate(${-1 * Math.floor(Math.random()*91)}deg);
                     left: ${posX}%;
                     top: ${posY}%;
                 '
             class='thoughtButton'
-            onclick="${i === randomButton ? ("location.href=\'" + './event' + randomEvent + '.html\'') : "location.reload()\""}
-            value="${randomWords[Math.floor(Math.random()*randomWords.length)]}" />`;
+            onclick="${j === randomButton ? ("location.href=\'" + './event' + randomEvent + '.html\'') : "location.reload()\""}
+             \/>`;
         tag.prepend(button);
         // console.log(tag);}
     }
+}
+
+function end() {
+    endNoEsc();
+}
+
+function endNoEsc() {
+    createInteractive("#intro", "#pageHeader_Content", endNoEscape);
+    // console.log(endNoEscape);
 }
--- project1/js/pages/menu.js
+++ project1/js/pages/menu.js
@@ -1,4 +1,5 @@
 export function menuYeet(pageName) {
-    $('body').last().addClass('_breatheBg');
-    $('#intro').last().addClass('_breathe');
+    // $('body').last().addClass('_breatheBg');
+    // $('#intro').last().addClass('_breathe');
+    console.log($(document));
 }
