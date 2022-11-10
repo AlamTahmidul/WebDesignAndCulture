874edd9eb13cf2261c3446c292865a8b2697b197
AlamTahmidul <mohammed.alam.217@gmail.com>
2022-11-10 00:28:53 -0500

    Added a timer and some  minor visual fixes

---
+++ alamTahmidul_project02/js/components/config.js
@@ -0,0 +1,17 @@
+/**
+ *  These are the game configurations that we can set to ensure balance
+ *  and also to play around with the values
+ *  
+ */
+
+
+/**
+ *  Time the player has before it expires...
+ */
+let timerDuration = 5;
+
+
+/**
+ *  Export all the variables (will be constants)
+ */
+export default timerDuration;
--- alamTahmidul_project02/js/index.js
+++ alamTahmidul_project02/js/index.js
@@ -1,40 +1,28 @@
 // import { CommentCard } from "./components/CommentCard.js"
+import timerDuration from "./components/config.js";
 
+let gameTimerDuration = timerDuration;
+let gameTimerHandler;
 window.onload = function () {
     // const comment = new CommentCard("Author", "This is a comment");
     // document.getElementById("wrapper-body").append(comment.toHTML());
     // document.body.innerHTML.append(comment.toHTML());
     // console.log(comment);
 
-    document.getElementById("view-tab").onmouseover= (event) => {
-        event.target.style.backgroundColor = "wheat";
-        event.target.style.color = "black";
-    }
-    document.getElementById("view-tab").onmouseout= (event) => {
-        event.target.style.backgroundColor = "transparent";
-        event.target.style.color = "wheat";
-    }
-
-    document.getElementById("window-close-btn").onmouseout= (event) => {
-        event.target.style.backgroundColor = "transparent";
-    }
-
-    document.getElementById("window-close-btn").onmouseover= (event) => {
-        event.target.style.backgroundColor = "red";
-    }
-
-    document.getElementById("window-minimize-btn").onmouseout= (event) => {
-        event.target.style.backgroundColor = "transparent";
-        event.target.style.opacity = "100%";
-    }
-
-    document.getElementById("window-minimize-btn").onmouseover= (event) => {
-        event.target.style.backgroundColor = "lightgray";
-        event.target.style.opacity = "70%";
-    }
-
     document.getElementById("url").onkeydown = (event) => {
         // TODO: See if you can load iFrames to serve multiple pages...
-        if (event.key === "Enter") document.getElementById("content-body").innerHTML = event.target.value;
+        if (event.key === "Enter") {
+            document.getElementById("content-body").innerHTML = event.target.value;
+            document.getElementById("tab-title").innerText = event.target.value;
+        }
     }
+
+    gameTimerHandler = setInterval(() => {
+        console.log(gameTimerDuration);
+        document.getElementById("timer-text").innerText = gameTimerDuration;
+        
+        if (--gameTimerDuration < 0) clearInterval(gameTimerHandler);
+    }, 1000);
+
+
 }
